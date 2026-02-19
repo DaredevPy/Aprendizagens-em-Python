af83307c4455058bbc64a357447ea051e990896a
DaredevPy <renatopiment@gmail.com>
2026-02-19 19:07:00 -0300

    Metodos_p_Manipular_Strings

---
+++ 18_Metodos_p_Manipular_Strings.py
@@ -0,0 +1,18 @@
+frase = "Eu adoro aprender Python na simplifica treinamentos"
+#Elimina a letra ou palavra que estiver dentro do parenteses
+print(frase.split("Python"))
+
+#Elimina os espaços em branco no inicio e no final da frase
+print(frase.strip("Python"))
+
+#Muda todas as letras para minuscula 
+print(frase.lower())
+
+#Muda todas as letras para maiuscula
+print(frase.upper())
+
+#Muda a primeira Letra da frase para maiuscula 
+print(frase.title())
+
+
+
