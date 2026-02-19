720bd1dd1587392e424b57f9af3322814c50744d
DaredevPy <renatopiment@gmail.com>
2026-02-19 19:02:45 -0300

    Tabuada

---
+++ 17_Tabuada.py
@@ -0,0 +1,7 @@
+numero = int(input("digite um numero"))
+contador = 1
+
+while contador <= 10:
+    resultado = numero * contador
+    print(f" {numero} * {contador} = {resultado}")
+    contador = contador +1 
