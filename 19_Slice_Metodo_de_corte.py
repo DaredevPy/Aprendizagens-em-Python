b285c7b0570bfd98b5e914dd148a72b9ab5b32ff
DaredevPy <renatopiment@gmail.com>
2026-02-19 19:07:37 -0300

    Slice_Metodo_de_corte

---
+++ 19_Slice_Metodo_de_corte.py
@@ -0,0 +1,9 @@
+
+# Exibe a strings da letra 0 ao numero Limite de letras da strings 0-1-2-3-->
+
+fruta = "Banana"
+print(fruta[slice(0, 2)])
+print(fruta[slice(0, 3)])
+print(fruta[slice(0, 4)])
+print(fruta[slice(0, 5)])
+print(fruta[slice(0, 6)])
