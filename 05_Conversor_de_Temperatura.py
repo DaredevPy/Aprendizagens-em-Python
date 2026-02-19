acdbaa5c8fe057b9382b445744d1a835eb4e937b
DaredevPy <renatopiment@gmail.com>
2026-02-19 18:38:00 -0300

    Conversor

---
+++ 05_Conversor_de_Temperatura.py
@@ -0,0 +1,11 @@
+#Exercicio 02 : Conversor de Temperatura
+
+#Entrada de dados
+celsius = float(input ("Digite a Temperatudo em Graus Celsius: "))
+
+#Processamento: Converter a Temperatura
+fahre = (celsius *9/5) + 32
+
+#Imprimir a conversao
+
+print(f"{celsius} C é igual a {fahre} F ")
