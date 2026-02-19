388af744bcf696c8678c18488427db2898749c70
DaredevPy <renatopiment@gmail.com>
2026-02-19 18:44:35 -0300

    EX_while

---
+++ 14_EX_while.py
@@ -0,0 +1,7 @@
+numero = 1
+while numero <= 5:
+    print(numero)
+    numero = numero + 1 
+print("Terminou o Programa")
+
+#Enquanto o numero for menor ou igual a 5, ele imprime a soma de numero = 1
