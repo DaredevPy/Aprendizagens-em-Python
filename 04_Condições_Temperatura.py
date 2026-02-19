c750fdd2e3931f702e70f47a49a1cab5d3a608a2
DaredevPy <renatopiment@gmail.com>
2026-02-19 18:37:40 -0300

    Temperatura

---
+++ 04_Condições_Temperatura.py
@@ -0,0 +1,8 @@
+temperatura = float (input ("Digite a temperatura "))
+
+if temperatura < 10:
+    print ("Esta Muito Frio!")
+elif 10 <= temperatura <20: # intervalo de 10 a 20
+    print("Esta Moderado")
+else:
+    print("Esta quente")    
